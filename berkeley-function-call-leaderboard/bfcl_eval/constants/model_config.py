8bd6ee00208a3c13840b8660ac6322a1b8862fab
Fred Reiss <frreiss@us.ibm.com>
2026-07-24 21:41:24 +0000

    Add Qwen 3.6 vLLM config

--- berkeley-function-call-leaderboard/bfcl_eval/constants/model_config.py
+++ berkeley-function-call-leaderboard/bfcl_eval/constants/model_config.py
@@ -1286,6 +1286,19 @@ api_inference_model_map = {
         is_fc_model=True,
         underscore_to_dot=True,
     ),
+    # IBM's config for Qwen 3.6 via vLLM, not part of official repo
+    "Qwen3.6-35B-A3B-FC": ModelConfig(
+        model_name="Qwen/Qwen3.6-35B-A3B",
+        display_name="Qwen3.6-35B-A3B (FC)",
+        url="",
+        org="Alibaba",
+        license="apache-2.0",
+        model_handler=VLLMCompletionsHandler,
+        input_price=0.0,
+        output_price=0.0,
+        is_fc_model=True,
+        underscore_to_dot=True,
+    ),
 }
 
 # Inference through local hosting
